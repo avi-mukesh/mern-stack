2ae9291cd51dcdaa0b83ed948ce80b48b4ed16c3
Avi Mukesh <avimukesh10@gmail.com>
2022-09-20 13:29:59 +0100

    added custom domain as allowed origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,6 @@
-const allowedOrigins = ["https://technotes-fyi9.onrender.com"]
+const allowedOrigins = [
+    "https://technotes-fyi9.onrender.com",
+    "https://technotes.avimukesh.com",
+]
 
 module.exports = allowedOrigins
